f04ca9f3d2e52073a01a4ff81743000aec02f7b7
75849479+Argavyon@users.noreply.github.com <75849479+Argavyon@users.noreply.github.com>
2023-07-31 14:59:12 +0200

    Improve Sort Safe Spells Spellgems in the faction safe are now compacted by spell and categorized They also allow easily picking the gem with most (or least) charges

--- modules/sortSafeSpells.js
+++ modules/sortSafeSpells.js
@@ -7,21 +7,90 @@ const sortSafeSpells = {
             'Sort Spellgems in Faction Safe.',
         );
 
-        const sortSafeSpells = () => {
+        const sortSafeSpells = async () => {
             const safeSpellButton = document.querySelector('form[name="footlockergrab"] input[value^="Retrieve Spell"]');
             if (!safeSpellButton) {
                 mod.debug('Retrieve Spell From Safe button not found.');
                 return;
             }
             const safeSpellSelect = safeSpellButton.parentNode.querySelector('select');
-            const sortedOpt = Array.from(safeSpellSelect.options).sort((a,b) => a.textContent < b.textContent ? -1 : 1);
+
+            const displayLeastCharged = await mod.getSetting('show-least-charged')
+
+            const mostCharged = {};
+            const leastCharged = {};
+            const notCharged = {};
+
+            // Spellgems display as either
+            // Acrid Vapors - Small Tan Gem, (2 charges) (1)
+            // Acrid Vapors - Small Gem, (2 charges) (1)
+
+            const SpellgemTextRegExp = /^(?<spell>.*) - Small( \w*)? Gem, \((?<charges>\d) charges\) \((?<amount>\d)\)$/;
+            Array.from(safeSpellSelect.options).forEach(opt => {
+                const { groups: groups } = SpellgemTextRegExp.exec(opt.text);
+                const spell = groups.spell;
+                const charges = parseInt(groups.charges);
+                const amount = parseInt(groups.amount);
+
+                if (charges > 0) {
+                    if (!(spell in mostCharged)) {
+                        mostCharged[spell] = { id: opt.value, charges: charges, amount: amount }
+                    } else {
+                        if (mostCharged[spell].charges < charges) {
+                            mostCharged[spell] = { id: opt.value, charges: charges, amount: mostCharged[spell].amount + amount }
+                        } else {
+                            mostCharged[spell].amount += amount;
+                        }
+                    }
+
+                    if (displayLeastCharged) {
+                        if (!(spell in leastCharged)) {
+                            leastCharged[spell] = { id: opt.value, charges: charges, amount: amount }
+                        } else {
+                            if (leastCharged[spell].charges > charges) {
+                                leastCharged[spell] = { id: opt.value, charges: charges, amount: leastCharged[spell].amount + amount }
+                            } else {
+                                leastCharged[spell].amount += amount;
+                            }
+                        }
+                    }
+                } else {
+                    if (!(spell in notCharged)) {
+                        notCharged[spell] = { id: opt.value, charges: charges, amount: amount }
+                    } else {
+                        notCharged[spell].amount += amount;
+                    }
+                }
+            });
+
             safeSpellSelect.innerHTML = '';
-            for (const opt of sortedOpt) safeSpellSelect.add(opt);
+
+            const displayedGroups = [];
+            displayedGroups.push([mostCharged, 'Most Charged Spellgems', '#75BFF0']);
+            if (displayLeastCharged) {
+                displayedGroups.push([leastCharged, 'Least Charged Spellgems', '#B6DDF7']);
+            }
+            displayedGroups.push([notCharged, 'Discharged Spellgems', '#F6FBFE']);
+            displayedGroups.forEach(([spellgems, groupName, groupColor]) => {
+                const group = safeSpellSelect.appendChild(document.createElement('optgroup'));
+                group.label = groupName;
+                group.style.backgroundColor = groupColor;
+                Object.entries(spellgems).forEach(([spell, { id: id, charges: charges, amount: amount }]) => {
+                    group.appendChild(new Option(`${spell} (${charges} charges) (${amount})`, id));
+                });
+            });
             safeSpellSelect.selectedIndex = 0;
         }
 
+        await mod.registerSetting(
+            'checkbox',
+            'show-least-charged',
+            'Display Least Charged spellgems',
+            ''
+        );
+
         await mod.registerMethod(
-            'sync',
+            'async',
             sortSafeSpells
         );
     }
